3e5e78e032ee35c9513062421c12935c0c19c9a9
gorobetc <96600005+gorobetc@users.noreply.github.com>
2021-12-24 03:20:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mtz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 3;
+            int y = 5;
+            Console.WriteLine(x + y);
+            Console.ReadKey();
+
+        }
+    }
+}
